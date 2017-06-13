30b32bfbe0052d6a52c2b9080786fdc0f493cf7e
Yaroslav Maximov <sharveywilliams@gmail.com>
2017-06-14 01:02:45 +0300

    update 14/06

--- News.py
+++ News.py
@@ -6,7 +6,7 @@ class News: #класс новостей
     def getGlobalNews(self, bot, update): # получить новости из мира
         r = requests.get("http://yandex.ru")
         soup = BeautifulSoup(r.text, "html.parser")
-        news = soup.find_all(class_="link list__item-content link_black_yes")
+        news = soup.find_all(class_="home-link list__item-content home-link_black_yes")
         for new_cur in news[0:5]:
             news_list = (new_cur.get("aria-label"))
             bot.sendMessage(chat_id=update.message.chat_id, text="• " + news_list)
@@ -15,7 +15,7 @@ class News: #класс новостей
     def getRegionNews(self,bot, update): # получить региональные новости
         r = requests.get("http://yandex.ru")
         soup = BeautifulSoup(r.text, "html.parser")
-        news = soup.find_all(class_="link list__item-content link_black_yes")
+        news = soup.find_all(class_="home-link list__item-content home-link_black_yes")
         for new_cur in news[5:10]:
             region_list = new_cur.get("aria-label")
             bot.sendMessage(chat_id=update.message.chat_id, text="• " + region_list)
